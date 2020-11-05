c002677752c4c69ff3a0cfa7dc807e8fdb4f0964
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-05 01:51:18 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
-CREATE TABLE character_books (id INTEGER PRIMARY KEY);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER);
 
 
 
